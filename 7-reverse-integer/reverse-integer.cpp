cc1d82ad9e1e2ff229946655d0db296e6faf31aa
Sudarshan Singh Udawat <107701857+udawat04@users.noreply.github.com>
2024-12-26 10:42:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (44.10%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -5,13 +5,15 @@ public:
         
         
             while(x){
+              
             rev =(rev*10)+(x%10);
             x/=10;
-        }
-        
-        if(rev>=INT_MAX || rev<=INT_MIN){
+              if(rev>=INT_MAX || rev<=INT_MIN){
             return 0;
         }
+        }
+        
+        
        
             return int(rev);
         
