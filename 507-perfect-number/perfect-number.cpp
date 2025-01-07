c947fd35cd343ca44f4020de6402c841eabc5816
Sudarshan Singh Udawat <107701857+udawat04@users.noreply.github.com>
2025-01-07 21:29:50 +0530

    Time: 1040 ms (53.84%) | Memory: 7.8 MB (15.58%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool checkPerfectNumber(int n) {
         int sum=0;
-      for(int i=1;i<n;i++){
+      for(int i=1;i<=n/2;i++){
         if(n%i==0){
             sum=sum+i;
         }
