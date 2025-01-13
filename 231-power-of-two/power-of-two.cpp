f072db4d95c4f193e7b139e717e009899cdfd3d3
Sudarshan Singh Udawat <107701857+udawat04@users.noreply.github.com>
2025-01-13 20:58:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (68.68%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==0){
+            return false;
+        }
+        
+        if(n%2==0){
+            while(n%2==0){
+                n/=2;
+            }
+        }
+        if(n==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
