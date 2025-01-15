658f337f97b11d32544178669b4978b1955c800e
Sudarshan Singh Udawat <107701857+udawat04@users.noreply.github.com>
2025-01-15 21:15:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (43.35%) - LeetSync

---
+++ 342-power-of-four/power-of-four.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0){
+            return false;
+        }
+        if(n%4==0){
+            while(n%4==0){
+                n/=4;
+            }
+        }
+        if(n==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
