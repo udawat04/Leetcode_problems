42c27987d5ca936a458d46112d88740a9b12872e
Sudarshan Singh Udawat <107701857+udawat04@users.noreply.github.com>
2025-02-01 20:19:23 +0530

    Time: 3 ms (16.92%) | Memory: 11.4 MB (81.45%) - LeetSync

---
+++ 979-di-string-match/di-string-match.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int n = s.size();
+        vector<int>perm(n + 1);
+        int low = 0;
+        int  high = n;
+        for (int i = 0; i < n;i++){
+            if(s[i] == 'I'){
+                perm[i] = low;
+                low++;
+            }else{
+                perm[i] = high;
+                high--;
+            }
+        }
+
+        perm[n] = low; 
+        return perm;
+    }
+};
